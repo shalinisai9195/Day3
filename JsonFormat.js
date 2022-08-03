ab0a7fd1edb6eb163ddbf8a0e869226414571310
shalinisai9195 <110511957+shalinisai9195@users.noreply.github.com>
2022-08-03 20:54:31 +0530

    Add files via upload

---
+++ JsonFormat.js
@@ -0,0 +1,16 @@
+
+//////////////////////// Json Format Resume //////////////////////////////////
+
+
+let resume = { name : "Thenmozhi L",
+Qualification : "BCA",
+year_of_Passedout : 2018,
+Address : { P_A : {No :123, street : "Ganapathy stree", city : "chennai", pincode: 600054},
+T_A : {No :12, street : "Raagav street", city : "chennai", pincode: 600101}
+
+},
+Hobbies : "Badminton",
+Contect_no : 9845768344
+}
+
+console.log(JSON.stringify(resume))
